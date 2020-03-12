758d78c1ea1ab98ac48706ffbe54b43cb7a7e8a0
Piet Geursen (via Travis CI) <pietgeursen@gmail.com>
2020-03-12 10:39:36 +0000

    Deploy sunrise-choir/ssb-db to github.com/sunrise-choir/ssb-db.git:gh-pages

--- implementors/snafu/trait.ErrorCompat.js
+++ implementors/snafu/trait.ErrorCompat.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["ssb_db"] = [{text:"impl ErrorCompat for <a class=\"enum\" href=\"ssb_db/error/enum.Error.html\" title=\"enum ssb_db::error::Error\">Error</a>",synthetic:false,types:["ssb_db::error::Error"]},];
+implementors["ssb_db"] = [{"text":"impl ErrorCompat for <a class=\"enum\" href=\"ssb_db/error/enum.Error.html\" title=\"enum ssb_db::error::Error\">Error</a>","synthetic":false,"types":["ssb_db::error::Error"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
